cc59cff2f16bf4b58caff91be31830dc01f57f55
Tim Hesse <tim.hesse@uni-oldenburg.de>
2022-04-22 16:49:45 +0200

    USE_LOADER context now allows strings

--- src/lib/http-context/use-loader.ts
+++ src/lib/http-context/use-loader.ts
@@ -2,9 +2,11 @@ import {HttpContextToken} from "@angular/common/http";
 
 /**
  * {@link HttpContextToken} used to determine if loader should be shown.
+ * A given string will be run through the translator and therefore may get
+ * translated.
  *
  * Useful for longer loading times.
  *
  * Defaults to `false`.
  */
-export const USE_LOADER = new HttpContextToken<boolean>(() => false);
+export const USE_LOADER = new HttpContextToken<string | boolean>(() => false);
