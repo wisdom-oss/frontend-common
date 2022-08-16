fc90fea76325491bbe75d2f70dd60b8d73d7a7e8
Tim Hesse <tim.hesse@uni-oldenburg.de>
2022-08-16 17:41:37 +0200

    optimized breadcrumbs

--- src/lib/breadcrumbs/breadcrumbs.component.ts
+++ src/lib/breadcrumbs/breadcrumbs.component.ts
@@ -1,23 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Breadcrumb, BreadcrumbsService} from "./breadcrumbs.service";
 import {ActivatedRouteSnapshot} from "@angular/router";
+import {takeWhile} from "rxjs";
+
+let counter = 0;
 
 @Component({
   selector: 'breadcrumbs',
   templateUrl: './breadcrumbs.component.html'
 })
-export class BreadcrumbsComponent implements OnInit {
+export class BreadcrumbsComponent implements OnInit, OnDestroy {
+  private alive: boolean = true;
   fragments: (Breadcrumb | undefined)[] = [];
 
   constructor(private service: BreadcrumbsService) { }
 
   ngOnInit(): void {
-    this.service.observe().subscribe(next => {
+    this.service.observe()
+      .pipe(takeWhile(() => this.alive))
+      .subscribe(next => {
       this.fragments = next
     });
   }
 
   isAllSet(): boolean {
     return !this.fragments.includes(undefined);
+  }
+
+  ngOnDestroy(): void {
+    this.alive = false;
   }
 }
