6fc44a3809c9f1a2d3b8b6809adceff54085ba51
Piepmatz <3590829+cptpiepmatz@users.noreply.github.com>
2023-05-17 16:59:44 +0200

    models can now be inserted from parent

--- src/lib/ifc/ifc.component.ts
+++ src/lib/ifc/ifc.component.ts
@@ -6,6 +6,34 @@ import {
   ViewChild
 } from '@angular/core';
 import {IfcViewerAPI} from "web-ifc-viewer";
+import {IFCModel} from "web-ifc-three/IFC/components/IFCModel";
+
+/**
+ * Model entry.
+ */
+type ModelEntry = {
+  /** Specifies the path to the IFC model file. */
+  path: string,
+
+  /**
+   * Indicates whether the model should be displayed after loading.
+   * This is optional, with the model being visible by default.
+   */
+  visible?: boolean,
+
+  /**
+   * Denotes whether this model's visibility can be altered.
+   * This is optional, with the model being disable-able by default.
+   */
+  fixed?: boolean,
+
+  /**
+   * Decides whether this model should be adjusted to fit within the frame.
+   * This is recommended to be set only for one model.
+   * This is optional, with the model not fitting to frame by default.
+   */
+  fitToFrame?: boolean
+};
 
 @Component({
   selector: 'ifc',
@@ -17,41 +45,47 @@ import {IfcViewerAPI} from "web-ifc-viewer";
 })
 export class IfcComponent implements AfterViewInit {
 
+  @Input()
+  height = "70vh";
+
+  @Input("models")
+  inputModels: Record<string, ModelEntry> = {};
+  models: Record<string, ModelEntry | {ifcModel: IFCModel}> = {}
+
   @ViewChild("viewerContainer")
   viewerContainer!: ElementRef<HTMLDivElement>;
 
   @ViewChild("resizeContainer")
   resizeContainer!: ElementRef<HTMLDivElement>;
 
-  @Input()
-  height = "70vh";
+  viewer!: IfcViewerAPI;
 
-  ngAfterViewInit(): void {
+  async ngAfterViewInit(): Promise<void> {
     const container = this.viewerContainer.nativeElement;
-    const viewer = new IfcViewerAPI({
-      container
-    });
-    viewer.IFC.loader.ifcManager.applyWebIfcConfig({
-      USE_FAST_BOOLS: true,
-      COORDINATE_TO_ORIGIN: true
-    }).catch(e => console.error(e));
-    viewer.IFC.loadIfcUrl("/files/WW-Langeoog/20191105-4001110-WW-TGA.ifc", true).then(() => {
-      viewer.IFC.loader.ifcManager.applyWebIfcConfig({
-        USE_FAST_BOOLS: true,
-        COORDINATE_TO_ORIGIN: false
-      }).then(() => {
-        viewer.IFC.loadIfcUrl("/files/WW-Langeoog/20191028-4001110-WW-Gel.ifc", true);
-        viewer.IFC.loadIfcUrl("/files/WW-Langeoog/20191028-4001110-WW-Arch.ifc", true);
-        viewer.IFC.loadIfcUrl("/files/WW-Langeoog/20191028-4001110-WW-ELT.ifc", true)
-      })
-    });
+    this.viewer = new IfcViewerAPI({ container });
+    await this.viewer.IFC.loader.ifcManager.useWebWorkers(true, "IFCWorker.js");
 
     const observer = new ResizeObserver(entries => {
       container.style.width = entries[0].contentRect.width + "px";
       container.style.height = entries[0].contentRect.height + "px";
-      viewer.context.updateAspect();
+      this.viewer.context.updateAspect();
     });
     observer.observe(this.resizeContainer.nativeElement);
+
+    let first = true;
+    for (
+      let [model, {path, visible, fixed, fitToFrame}]
+      of Object.entries(this.inputModels)
+    ) {
+      await this.viewer.IFC.loader.ifcManager.applyWebIfcConfig({
+        USE_FAST_BOOLS: true,
+        COORDINATE_TO_ORIGIN: first
+      });
+      first = false;
+      const ifcModel = await this.viewer.IFC.loadIfcUrl(path, fitToFrame);
+      if (visible === false) this.viewer.context.scene.removeModel(ifcModel);
+      this.models[model] = {path, visible, fixed, ifcModel};
+    }
   }
 
 }
