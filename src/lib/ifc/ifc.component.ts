00489d3a9bd1132baa6f75819ace9664a0850449
Piepmatz <3590829+cptpiepmatz@users.noreply.github.com>
2023-05-17 14:24:19 +0200

    added primitive logic to allow rendering our ifc

--- src/lib/ifc/ifc.component.ts
+++ src/lib/ifc/ifc.component.ts
@@ -1,10 +1,18 @@
-import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Input,
+  ViewChild
+} from '@angular/core';
 import {IfcViewerAPI} from "web-ifc-viewer";
 
 @Component({
   selector: 'ifc',
   template: `
-    <div #viewerContainer></div>
+    <div #resizeContainer style="width: 100%; height: {{height}}">
+      <div #viewerContainer style="position: absolute"></div>
+    </div>
   `,
 })
 export class IfcComponent implements AfterViewInit {
@@ -12,19 +20,38 @@ export class IfcComponent implements AfterViewInit {
   @ViewChild("viewerContainer")
   viewerContainer!: ElementRef<HTMLDivElement>;
 
+  @ViewChild("resizeContainer")
+  resizeContainer!: ElementRef<HTMLDivElement>;
+
+  @Input()
+  height = "70vh";
+
   ngAfterViewInit(): void {
     const container = this.viewerContainer.nativeElement;
     const viewer = new IfcViewerAPI({
       container
     });
-    viewer.axes.setAxes();
-    viewer.grid.setGrid();
     viewer.IFC.loader.ifcManager.applyWebIfcConfig({
       USE_FAST_BOOLS: true,
       COORDINATE_TO_ORIGIN: true
     }).catch(e => console.error(e));
-    viewer.context.renderer.postProduction.active = true;
-    viewer.IFC.loadIfcUrl("/files/WW-TGA.ifc");
+    viewer.IFC.loadIfcUrl("/files/WW-Langeoog/20191105-4001110-WW-TGA.ifc", true).then(() => {
+      viewer.IFC.loader.ifcManager.applyWebIfcConfig({
+        USE_FAST_BOOLS: true,
+        COORDINATE_TO_ORIGIN: false
+      }).then(() => {
+        viewer.IFC.loadIfcUrl("/files/WW-Langeoog/20191028-4001110-WW-Gel.ifc", true);
+        viewer.IFC.loadIfcUrl("/files/WW-Langeoog/20191028-4001110-WW-Arch.ifc", true);
+        viewer.IFC.loadIfcUrl("/files/WW-Langeoog/20191028-4001110-WW-ELT.ifc", true)
+      })
+    });
+
+    const observer = new ResizeObserver(entries => {
+      container.style.width = entries[0].contentRect.width + "px";
+      container.style.height = entries[0].contentRect.height + "px";
+      viewer.context.updateAspect();
+    });
+    observer.observe(this.resizeContainer.nativeElement);
   }
 
 }
